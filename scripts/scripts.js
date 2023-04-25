8c91240112b299b2ae59dd40d29fe5b1f7047639
makintosh00 <nm00@bu.edu>
2023-04-25 19:30:44 -0400

    changed layout

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,19 +1,22 @@
 /* WRITE YOUR JS HERE... YOU MAY REQUIRE MORE THAN ONE JS FILE. IF SO SAVE IT SEPARATELY IN THE SCRIPTS DIRECTORY */
+
+
+
 const ingredients = Array.from(document.getElementsByName('ingChoice')); 
 
 const budget = Array.from(document.getElementsByName('bgtChoice'));
 
 const diet = Array.from(document.getElementsByName('dietChoice'));
 
-const header = document.getElementById('headerBox');
+const stCtrl = document.querySelector('.startcontrols');
 const ingredientBtn = document.getElementById('ing-btn');
 const recipeBtn = document.getElementById('rec-btn');
 const sections = Array.from(document.getElementsByClassName('promptcontainer'));
-const firstQuestion = document.getElementById('qIngredient');
-
+const slideContainer = document.querySelector('.slideBox');
+const slider = document.querySelector('.slider');
 const resultBtn = document.getElementsByName('result-btn');
+const recSlide = document .querySelector('#recSlide');
 
-const nextBtn =document.getElementsByName('next-btn');
 
 
 
@@ -22,23 +25,25 @@ for (var i = 0 ; i < resultBtn.length; i++) {
 }
 
 ingredientBtn.addEventListener('click' , startIngredient);
-nextBtn.addEventListener('click', clickNext) ;
+recipeBtn.addEventListener('click',startRecipe );
 
 function startIngredient(){
-    header.classList.add('hide');
-    firstQuestion.classList.remove('hide');
+    stCtrl.classList.add('hide');
+    slideContainer.classList.remove('hide');
+    $('.slider').slick('refresh');
+    slider.style.cssText = `transform: translateY(-250px); transition: .5s`; 
+
+}
+
+function startRecipe(){
+  stCtrl.classList.add('hide');
+  recSlide.classList.remove('hide');
+  $('.slider').slick('refresh');
+  recSlide.style.cssText = `transform: translateY(-250px); transition: .5s`; 
+
 }
 
 
-function clickNext(){
-    let i =0; 
- while (i <sections.length) {
-   let next = document.getElementsByClassName('promptcontainer').nextSibling;
-   next.classList.remove('hide');
-   next = next.nextSibling;
-   i++; 
- }
-}
 
 
 function getResult(){
@@ -69,3 +74,5 @@ ingredients.forEach(item => {
   console.log(result); 
 
   }
+
+
