e514ac73e27747e3acfb4a9e307c2cfa1732bb7f
makintosh00 <nm00@bu.edu>
2023-05-01 11:09:31 -0400

    update

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -8,7 +8,11 @@ const budget = Array.from(document.getElementsByName('bgtChoice'));
 
 const diet = Array.from(document.getElementsByName('dietChoice'));
 
+const choices = document.querySelectorAll('.checkmark');
+
 const stCtrl = document.querySelector('.startcontrols');
+const siteName = document.querySelector('#siteName');
+const subhead = document.querySelector('#subheading');
 const ingredientBtn = document.getElementById('ing-btn');
 const recipeBtn = document.getElementById('rec-btn');
 const sections = Array.from(document.getElementsByClassName('promptcontainer'));
@@ -20,36 +24,53 @@ const recSlide = document .querySelector('#recSlide');
 
 
 
-for (var i = 0 ; i < resultBtn.length; i++) {
+for (var i = 0 ; i < resultBtn.length; i++) {     // from https://stackoverflow.com/a/32027957
    resultBtn[i].addEventListener('click', getResult ) ; 
+}
+
+for (var i = 0 ; i < choices.length; i++) {
+  choices[i].addEventListener('click', highlight ) ; 
 }
 
 ingredientBtn.addEventListener('click' , startIngredient);
 recipeBtn.addEventListener('click',startRecipe );
 
+
+
 function startIngredient(){
-    stCtrl.classList.add('hide');
+    
+    subhead.classList.add('hide');
     slideContainer.classList.remove('hide');
     $('.slider').slick('refresh');
-    slider.style.cssText = `transform: translateY(-250px); transition: .5s`; 
-
+    slider.style.cssText = `transform: translateY(-200px); transition: .5s`; 
+    siteName.style.cssText = `opacity:50%; transition: .5s`; 
 }
 
 function startRecipe(){
-  stCtrl.classList.add('hide');
+  subhead.classList.add('hide');
   recSlide.classList.remove('hide');
-  $('.slider').slick('refresh');
-  recSlide.style.cssText = `transform: translateY(-250px); transition: .5s`; 
-
+  $('.slider').slick('refresh'); // this is a slick setting to refresh the slide after a javascript cue 
+  recSlide.style.cssText = `transform: translateY(-200px); transition: .5s`; 
+  siteName.style.cssText = `opacity:50%; transition: .5s`;
 }
 
 
+function highlight(item){
+ 
+      if (item.checked){
+        console.log(item.value);
+        item.style.backgroundColor = "red";
+      }
+    
+  }; 
+
+  
 
 
 function getResult(){
   const result = {
     ingredients:[],
-    budget: [],
+    budget:[],
     diet:[],
   };
   
@@ -72,6 +93,7 @@ ingredients.forEach(item => {
   }); 
   
   console.log(result); 
+
 
   }
 
